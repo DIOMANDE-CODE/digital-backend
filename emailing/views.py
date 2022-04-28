47dd606406e17cd9f18a39c2fa0b307f01442bee
DIOMANDE-CODE <diomandedroh79@gmail.com>
2022-04-29 01:47:43 +0200

    save contact

--- emailing/views.py
+++ emailing/views.py
@@ -19,14 +19,14 @@ class EmailingList(APIView):
     def post(self, request, format=None):
         serializer = EmailingSerializer(data=request.data)
         if serializer.is_valid():
-            send_mail = EmailMessage(
-                serializer.data['sujet'],
-                serializer.data['message'],
-                serializer.data['mail'],
-                [settings.EMAIL_HOST_USER],
-                reply_to= (serializer.data['mail'],)
-            )
-            send_mail.send()
-            # serializer.save()
+            # send_mail = EmailMessage(
+            #     serializer.data['sujet'],
+            #     serializer.data['message'],
+            #     serializer.data['mail'],
+            #     [settings.EMAIL_HOST_USER],
+            #     reply_to= (serializer.data['mail'],)
+            # )
+            # send_mail.send()
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
